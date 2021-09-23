5fbf93e1a027343af2d78d1e9abd5eb3e3b82aa6
ZGaryVal <90227266+ZGaryVal@users.noreply.github.com>
2021-09-22 23:05:01 -0400

    Falta diseño de card reportero

--- Cliente/src/app/detallescuenta/detallescuenta.component.ts
+++ Cliente/src/app/detallescuenta/detallescuenta.component.ts
@@ -20,6 +20,14 @@ export class DetallescuentaComponent implements OnInit {
     
   }
 
+  nombres:string;
+  paterno:string;
+  materno:string;
+  sexo:string;
+  cargo:string;
+  ci:string;
+  habil:boolean;
+
   async recuperarUser():Promise<void>{
     let cuentas=null;
     //await this.http.get(this.serverDirection+`/probe`);
@@ -27,6 +35,13 @@ export class DetallescuentaComponent implements OnInit {
     .then((res:any)=>{
       this.cuentaRecuperada=res
       console.log(this.cuentaRecuperada);
+      this.nombres=this.cuentaRecuperada.nombres;
+      this.paterno=this.cuentaRecuperada.apepaterno;
+      this.materno=this.cuentaRecuperada.apematerno;
+      this.sexo=this.cuentaRecuperada.sexo;
+      this.cargo=this.cuentaRecuperada.cargo;
+      this.ci=this.cuentaRecuperada.ci;
+      this.habil=this.cuentaRecuperada.habilitada;
     });
   }
   mostrarID(){
